df9e547f8f8fb4460b002e40b7dbffc96a75a28d
chibatakashi <nixon0290211@icloud.com>
2022-11-09 06:53:31 +0900

    ギブアップ

--- app.py
+++ app.py
@@ -1,37 +1,79 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, request_finished
 from process import paste_frame
-import secrets
+import secrets,json
 
 app = Flask(__name__)
-
 # セッション情報を暗号化するためのキー設定
 app.secret_key = secrets.token_urlsafe(32)
 
 
+def cat():
+    response = requests.get("https://thecatapi.com/”)
+    # 返ってきたデータ(文字)をそのまま表示
+    print(response.text)
+    #jsonデータとしてロードして特定のデータのみ
+    print(json.loads(response.text)[“results”][0][“address1”])
+def dog():
+    response = requests.get("https://dog.ceo/api/breeds/image/random”)
+    # 返ってきたデータ(文字)をそのまま表示
+    print(response.text)
+    #jsonデータとしてロードして特定のデータのみ
+    print(json.loads(response.text)[“results”][0][“address1”])
+def fox():
+    response = requests.get("https:\/\/randomfox.ca\/?i=22”)
+    # 返ってきたデータ(文字)をそのまま表示
+    print(response.text)
+    #jsonデータとしてロードして特定のデータのみ
+    print(json.loads(response.text)[“results”][0][“address1”])
+
 @app.route("/")
-def show_form():
+def show_from():
     return render_template("index.html")
 
-
-@app.route("/upload", methods=["GET", "POST"])
+@app.route("/post", methods=["GET,POST"])
 def upload_image():
     if request.method == "POST":
-        if "file" not in request.files:
-            flash("ファイルが選択されていません")
-            return redirect(request.url)
+        animal = request.form["animal"]
+        if animal == "cat":
+            outanimal = api_get.cat()
 
-        file = request.files["file"]
+        elif animal == "dog":
+            outanimal = api_get.dog()
 
-        if file.filename == "":
-            flash("ファイルが選択されていません")
-            return redirect(request.url)
+        elif animal == "fox":
+            outanimal = api_get.fox()
 
-        if file:
-            outfile = paste_frame(file)
-            return render_template("upload.html", outfile=outfile)
+        else:
+            outanimal = ""
 
-    return redirect("/")
+        return render_template("post.html", outanimal=outanimal)
+
+    # return redirect("/")
+    # if request.method == "GET":
+    #     posts = Post.query.all() 
+    # return redirect(index.html,post =post)
+
+    #     file = request.files["file"]
+    #     else:title =request.from.get(title)
+
+
+    #     if file.filename == "":
+    #         flash("ファイルが選択されていません")
+    #         return redirect(request.url)
+
+    #     if file:
+    #         outfile = paste_frame(file)
+    #         return render_template("upload.html", outfile=outfile)
+
+    # return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+
+# If 猫
+# 　return “猫”
+# ElseIf 犬
+#    Return “犬”
+# Else  狐
+#    Return “狐”
--- process.py
+++ process.py
@@ -2,11 +2,14 @@ from PIL import Image
 
 
 def paste_frame(file):
+
     frame_path = "static/images/frame.png"
     output_path = "static/images/out.png"
     img = Image.open(file)
     frame = Image.open(frame_path)
+
     resized_frame = frame.resize((img.width, img.height))
     img.paste(resized_frame, (0, 0), resized_frame)
+
     img.save(output_path)
     return output_path
